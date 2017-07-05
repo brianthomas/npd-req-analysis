3a8fac18e94b004c3a9025a8f0d45dbc583bb133
Brian Thomas <brian.a.thomas@nasa.gov>
2017-07-05 16:54:06 -0400

    first working version reading(paging) thru PDF document

---
+++ bin/find_ndl_doc_req_dist.py
@@ -0,0 +1,109 @@
+''' 
+   Find the Normalized Damerau Levenshtein distance between a PDF document 
+   and individual requirements held in a spreadsheet.
+
+   Created on July 5, 2017
+
+   @author: thomas
+'''
+
+def _key(index1, index2):
+    if index1 >= index2:
+        return str(index2) + '_' + str(index1)
+    else:
+        return str(index1) + '_' + str(index2)
+
+def find_distance(pages, reqs):
+    from pyxdameraulevenshtein import damerau_levenshtein_distance, normalized_damerau_levenshtein_distance
+
+    # print("RUNNING on "+str(len(reqs))+" requirements") 
+    results = {} 
+    id_req = 2
+    for req in reqs:
+        id_page = 1
+        for p in pages:
+            key = _key(id_req, id_page)
+            if key not in results.keys():
+ 
+                req_txt = ""
+                if req['content']: 
+                    req_txt = req['content'].strip()
+                page_txt = ""
+                if p: 
+                    page_txt = p.strip()
+                if req_txt == "" and page_txt == "": 
+                    # trivial case, NO text to cross compare
+                    pass
+                else:
+                    results[key] = normalized_damerau_levenshtein_distance(req_txt, page_txt)
+            id_page = id_page + 1
+        id_req = id_req + 1
+             
+    return results
+
+def parse_doc_to_pages(doc): 
+    import PyPDF2
+
+    # open the document 
+    f = open(doc, 'rb')
+    document = PyPDF2.PdfFileReader(f)
+
+    #parse it for paragraphs, capturing metadata about document location they are in
+    pages = []
+
+    for p in range(0,document.getNumPages()):
+        page = document.getPage(p)
+        pages.append(page.extractText())
+        
+
+    f.close()
+    #print (pages[0])
+    return pages  
+
+if __name__ == '__main__':
+    import argparse
+    import operator
+    from npd.parser import parse 
+
+    # Use nargs to specify how many arguments an option should take.
+    ap = argparse.ArgumentParser(description='XLSM requirements db loader')
+    ap.add_argument('-d', '--doc', type=str, help='Name of the document (NPR/NPD) we want to compare to requirements')
+    ap.add_argument('-r', '--reqs', type=str, help='Name of the Excel file which holds the data')
+    ap.add_argument('-X', '--excel', type=bool, default=False, help='Whether req file is in Excel format')
+
+    # parse argv
+    opts = ap.parse_args()
+
+    if not opts.doc:
+        print ("the --doc <file> parameter must be specified")
+        ap.print_usage()
+        exit()
+
+    if not opts.reqs:
+        print ("the --reqs <file> parameter must be specified")
+        ap.print_usage()
+        exit()
+
+    reqs = []
+    if opts.excel:
+        reqs = parse(opts.reqs)
+    else:
+        with open(opts.reqs, 'r') as f:
+            for item in f.readlines():
+                reqs.append({'content':item}) 
+
+    # load/parse pages from document
+    pages = parse_doc_to_pages(opts.doc)
+
+    # calc distances now
+    distances = find_distance(pages, reqs)
+
+    # print it out
+    for result in sorted(distances.items(), key=operator.itemgetter(1)):
+        cols = [int(item) for item in result[0].split('_')]
+        cols.append(result[1])
+        print (', '.join(str(x) for x in cols))
+        # DEBUG comparison by showing compared lines in situ of output
+  #      print ('    ' + data[cols[0]-2]['content'] + " VS " + data[cols[1]-2]['content']) 
+
+
